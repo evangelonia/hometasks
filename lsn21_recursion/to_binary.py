0c0ea5c87650cd6195ea1ebc3e821ff6012c174b
evangelonia <55499978+evangelonia@users.noreply.github.com>
2019-10-28 13:47:07 +0200

    Create to_binary.py

---
+++ lsn21_recursion/to_binary.py
@@ -0,0 +1,14 @@
+"""Make a function that takes an integer (in decimal form)
+and convert it to a binary form using recursion  """
+
+
+def to_binary_func(n):
+    res, ost = n % 2, n // 2
+    if ost in [1, 0]:
+        print(res, ost)
+        return f"{ost}{res}"
+    print(res)
+    return to_binary_func(ost) + str(res)
+
+
+print(to_binary_func(5))
