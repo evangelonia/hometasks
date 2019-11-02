63678ad71feb671186c9b3a6884f1bed4b155ebc
evangelonia <55499978+evangelonia@users.noreply.github.com>
2019-11-02 11:37:42 +0200

    Update scarping_new.py

--- scarping_new.py
+++ scarping_new.py
@@ -21,13 +21,10 @@ def filter_parent(pid=0):
 
 
 def print_li(dict1, lvl=0):
-    # вывели открывающуюся лишку
     pr_li = f"""\t<li>{dict1["name"]}"""
     pr_li += f"""\t<li><a>{dict1["lnk"]}"""
     pr_li += "</a>\n"
-    # Выведем если есть дети
     pr_li += print_ul(dict1['id'], lvl)
-    # закрыли лишку
     pr_li += "</li>\n"
     pr_li += "</li>\n"
 
@@ -36,28 +33,20 @@ def print_li(dict1, lvl=0):
 
 def print_ul(pid, lvl):
     pr_ul = ""
-    # получили детей
     deti = filter_parent(pid)
-    # если есть дети
     if len(deti) > 0:
-        # открыли улку
         pr_ul += f"\n<ul class='level{lvl + 1}'>"
-        # в цикле по детям вызвали вывод их лишки
         for i in deti:
             pr_ul += print_li(i, lvl + 1)
-        # закрыли улку
         pr_ul += "</ul>\n"
-    # вернули собранный текст
     return pr_ul
 
 
 if __name__ == "__main__":
 
-    # прочитали в переменную
     read_in_list("leftmenu.csv")
     print(menu)
     # print(filter_parent(4))
 
-    # Вводим нулевой уровень
     mnu = print_ul(0, 0)
     print(mnu)
